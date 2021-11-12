2ab6d1a8b45051682e4ec8036b14a823bf8dfa6e
faheemjailor123 <93036039+faheemjailor123@users.noreply.github.com>
2021-11-12 22:06:43 +0530

    UC1

---
+++ program.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace lambda.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> list = new List<Person>();
+            Console.ReadLine();
+        }
+        public static void AddPersonalDetailsIntoList(List<Person>);
+        {
+            Person p = new Person() { };
+            list.Add(new Person() { SSN = 1, Name = "Shrikant ", Address  "India", Age = 14 });
+            list.Add(new Person() { SSN = 2, Name = "Arun ", Address  "mumbai", Age = 45});
+            list.Add(new Person() { SSN = 3, Name = "Ajay ", Address  "pakistan", Age = 16 });
+            list.Add(new Person() { SSN = 4, Name = "Mohan ", Address  "bangladesh", Age = 25 });
+            list.Add(new Person() { SSN = 5, Name = "sachine ", Address  "bengaluru", Age = 32 });
+            list.Add(p);
+
+        }
+    }
+}
